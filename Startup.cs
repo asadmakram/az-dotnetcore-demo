f874ff2da4661f63a84009f3c1c59afe59bf1eb8
Asad Mehmood Akram <asad.akram@contour-software.com>
2020-08-07 23:22:09 +0500

    updated hello message

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace az_dotnetcore_demo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!");
                 });
             });
         }
